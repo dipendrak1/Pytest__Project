62e2f63b85903cb4f59fff8195c630e9120a970e
kumardip <dipendrakumar932@gmail.com>
2023-04-19 11:09:40 +0530

    Added function add_new_row_to_df(local_spark, local_df, rows_to_append)

--- main.py
+++ main.py
@@ -20,3 +20,9 @@ def sum_of_column_on_partition(local_df, partition_by_cols_list, col_name):
     window = Window.partitionBy(partition_by_cols_list)
     local_df = local_df.select('*', f.sum(f.col(col_name)).over(window).alias('sum_column'))
     return local_df
+
+
+def add_new_row_to_df(local_spark, local_df, rows_to_append):
+    """rows_to_append example format = [(1, 2, 3, 'a'), (4, 5, 6, 'b')]"""
+    new_row = local_spark.createDataFrame(rows_to_append, local_df.columns)
+    return local_df.union(new_row)
